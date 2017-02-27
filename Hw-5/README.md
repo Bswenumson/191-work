e9c9625ef1ed1b4aa4bc9b8014321c0f9d5866d5
Brooke Swenumson <bswenumson@gmail.com>
2017-02-26 23:25:01 -0700

    Hw5

---
+++ Hw-5/README.md
@@ -0,0 +1,23 @@
+# Random Math
+### Brooke Swenumson
+
+This assignment was pretty difficult. I am just barely beginning to grasp and remember the various math functions.  I tried various combinations for about four hours before ending up with my final product.
+
+### Work and Problems
+
+I put these two categories together for this assignment since my work involved me trying new things, seeing what the problem was and trying to figure it out.
+
+### Breakthroughs
+
+I didn't have any full-fledged 'ah-ha!' moments on this assignment, it is a bit muddy for me still.
+
+### Results
+
+I am fairly pleased with my results, just for the fact that it works and looks pretty. However, I had a hard time controlling or fully understanding the process.
+
+### Future work
+
+I know practice is key. The more I work on it the better I will get. And the more tools I will be able to utilize.
+
+![Hw_5a](./Imgs/Hw_5a.png)
+![Hw_5b](./Imgs/Hw_5b.png)
---
+++ Hw-5/sketch.js
@@ -0,0 +1,78 @@
+// Define Global Variables
+    var col = {
+      r: 100,
+      g: 100,
+      b: 100
+    };
+    var valX = 0;
+    var valY = 0;
+    var valX_2 = 100;
+    var valY_2 = 100;
+    var midi = 20;
+    var midiCat = 30;
+    var xavier = 15;
+    var floof = 25;
+
+
+
+function setup() {
+    createCanvas(800,800);
+    var currFrameRate;
+    currFrameRate = frameRate(25);
+    background(10);
+
+
+
+}
+
+function draw() {
+
+    blendMode(OVERLAY);
+    noStroke();
+    fill(col.r, col.g, col.b);
+    arc(valX, valY, midi, midiCat, 0, PI+QUARTER_PI, OPEN);
+
+    valX = noise(midi) * width;
+    valY = random(0,height);
+    midi = midi + 1;
+    midiCat = abs(midi * 0.5);
+
+    col.r = random (20, 150);
+    col.g = random (100, 200);
+    col.b = random (100,190);
+
+    if (frameCount % 1500 === 0){midi = 20;}
+
+
+    push();
+    // blendMode(SCREEN);
+    fill(col.r,col.g,col.b);
+    noStroke();
+    ellipse(valX_2, valY_2, xavier, floof);
+
+    valX_2 = noise(xavier) * width;
+    valY_2 = random(0,height);
+    xavier = xavier + 0.001;
+    floof = floof - 0.001;
+
+    pop();
+
+
+
+    // if (frameCount % 100 === 0){valY = 100};
+
+/*
+  push();
+  stroke(col.r,col.g,col.b);
+  line(0,800,valX_2,valY_2);
+
+  col.r = random (100,180);
+  col.g = random(0,100);
+  col.b = random(100,200);
+
+  valX_2 = random (0, width);
+  valY_2 = random(0,height);
+  pop();
+*/
+
+}
