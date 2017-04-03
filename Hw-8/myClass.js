4ab633c2899ee2ed14a2be7b499ce102036c1be6
Brooke Swenumson <bswenumson@gmail.com>
2017-04-02 22:23:10 -0600

    Hw-8

---
+++ Hw-8/myClass.js
@@ -0,0 +1,25 @@
+// Define my class... I didn't know what to call it so it became Zip
+function Zip(){
+    this.x = random(0, 800);
+    this.y = random(0, 800);
+    this.size = random(5, 50);
+    this.r = random(0, 75);
+    this.g = random(100, 200);
+    this.b = random(150, 250);
+    this.speed = random(0.25 , 1);
+
+    this.display = function() {
+      fill(this.r, this.g, this.b);
+      ellipse(this.x, this.y, this.size, this.size);
+      fill(0);
+      ellipse(this.x, this.y, this.size * 0.25, this.size * 0.25);
+    };
+
+    this.move = function() {
+      // this.x = this.x + this.speed;
+      this.y = this.y - this.speed;
+      if (this.y < 0 - this.size * 0.5){
+        this.y = height;
+      }
+    };
+}
---
+++ Hw-8/sketch.js
@@ -0,0 +1,43 @@
+var zip = [];
+
+function setup() {
+    createCanvas(800, 800);
+    for (var i = 0; i < 500; i++){
+      zip[i] = new Zip();
+    }
+}
+
+function draw() {
+    background(0);
+    for (var i = 0; i < 500; i++) {
+      zip[i].move();
+      zip[i].display();
+    }
+}
+
+/*function Zip(){
+    this.x = random(0, 800);
+    this.y = random(0, 800);
+    this.size = random(5, 50);
+    this.r = random(0, 75);
+    this.g = random(100, 200);
+    this.b = random(150, 250);
+    this.speed = random(0.25 , 1);
+
+    this.display = function() {
+      fill(this.r, this.g, this.b);
+      ellipse(this.x, this.y, this.size, this.size);
+      fill(0);
+      ellipse(this.x, this.y, this.size * 0.25, this.size * 0.25);
+    };
+
+    this.move = function() {
+      // this.x = this.x + this.speed;
+      this.y = this.y - this.speed;
+      if (this.y < 0 - this.size * 0.5){
+        this.y = height;
+      }
+
+    };
+}
+*/
